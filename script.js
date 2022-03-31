ff9324889d5754442f379880a9adb0d58ec125db
CydaCode <nwacynti25@gmail.com>
2022-03-31 10:13:32 -0700

    was able to add a click event

--- script.js
+++ script.js
@@ -1,35 +1,89 @@
+const sounds = [
+    'clap',
+    'hihat',
+    'kick',
+    'openhat',
+    'boom',
+    'snare',
+    'tom',
+    'tink',
+    'ride',
+]
 
-
-function playSound(e) {
-    let keyCodes;
-    if (e.type === "click") {
-        keyCodes = e.currentTarget.dataset.key;
-        this.classList.add("playing")
-        console.log(keyCodes);
-        audio.currentTime = 0;
-        audio.play();
-    } else {
-        keyCodes = e.keyCode;
-    }
+const keyletters = [
+    'J',
+    'Q',
+    'M',
+    'P',
+    'C',
+    'O',
+    'Z',
+    'Y',
+    'T',
+]
+const allkey = document.querySelector(".keys")
+sounds.forEach((soundss) => {
+    const btn = document.createElement("div");
+    btn.classList.add('key');
     
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
-    const key = document.querySelector(`div[data-key="${e.keyCode}"]`)
-        if(!audio) {
-            return
-        }
-        audio.play();
-        key.classList.add("playing")
+
+    const keyb = document.createElement("kbd");
+    keyb.innerText = soundss
+    btn.appendChild(keyb);
+    allkey.appendChild(btn)
+
+    btn.addEventListener("click", () => {
+        document.getElementById(soundss).play()
+    })
+
+    keyletters.forEach((keyr) => {
+        return keyr
+    })
+})  
+
+
+
+
+// function playSound(e) {
+    
+    
+//     var audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+//     var key = document.querySelector(`div[data-key="${e.keyCode}"]`)
+//         if(!audio) return;
+        
+//         audio.currentTime = 0;
+//         audio.play();
+//         console.log( "second", audio)
+//         key.classList.add("playing")
+        
   
-}
-function removePlaying(e) {
-    if(e.propertyName != "transform") return;
-    this.classList.remove("playing")
-}
+// }
 
-const keys = Array.from(document.querySelectorAll(".key"));
+// function lll(e) {
+//     var audios = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+//     let keyCodes;
+//     if (e.type === "click") {
+//         keyCodes = playSound(e)
+//         console.log( "first", keyCodes)
+//         this.classList.add("playing")
+        
+       
 
-    keys.forEach(key => key.addEventListener("transitionend", removePlaying))
-    keys.forEach(key => key.addEventListener("click", playSound));
-    window.addEventListener("keydown", playSound)
+//     } else {
+//         keyCodes = e.keyCode;
+//         console.log("third", keyCodes)
+       
+//     }
+// }
+// function removePlaying(e) {
+//     if(e.propertyName != "transform") return;
+//     this.classList.remove("playing")
+// }
+
+// const keys = Array.from(document.querySelectorAll(".key"));
+
+//     keys.forEach(key => key.addEventListener("transitionend", removePlaying))
+//     keys.forEach(key => key.addEventListener("click", lll));
+//     window.addEventListener("keydown", playSound)
 
 
